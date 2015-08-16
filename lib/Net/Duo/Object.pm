4950cd4caa9c7b25882fd02404dc2ca3c8258f65
Russ Allbery <rra@cpan.org>
2015-08-16 10:30:47 -0700

    Use JSON::true and JSON::false when encoding
    Adjust for an incompatible change in JSON::XS 3.0 that broke magical
    interpretation of the strings "true" and "false" when encoding JSON.
    Use JSON::true and JSON::false instead to ensure that the JSON is
    created properly.

--- lib/Net/Duo/Object.pm
+++ lib/Net/Duo/Object.pm
@@ -288,7 +288,7 @@ sub json {
         my ($type, $flags) = _field_type($fields->{$field});
         if ($type eq 'simple' || $type eq 'array') {
             if ($flags->{boolean}) {
-                $data{$field} = $self->{$field} ? 'true' : 'false';
+                $data{$field} = $self->{$field} ? JSON::true : JSON::false;
             } elsif ($flags->{zero_or_one}) {
                 $data{$field} = $self->{$field} ? 1 : 0;
             } else {
@@ -479,6 +479,8 @@ JSON APIs.
 Russ Allbery <rra@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
+
+Copyright 2015 Russ Allbery <rra@cpan.org>
 
 Copyright 2014 The Board of Trustees of the Leland Stanford Junior
 University
